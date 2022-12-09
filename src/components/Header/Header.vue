b9927ecc5c25b5b5f6610b740866c0cd32cec037
Marios Kourouklis <Marios.Kourouklis@awacademy.training>
2022-12-09 11:50:54 +0200

    3

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+
+<template>
+  <header class="has-background-danger ">
+    <img src="@/assets/logo.png" alt="Pokemon Logo" width="300" height="300" >
+
+    <h1 class="title is-1 has-text-centered  ">My Awesome Pokemon List</h1>
+  </header>
+
+</template>
+
+
+
+<style scoped>
+header{
+  padding:80px 80px 80px 80px;
+
+}
+img{
+  padding:0px 0px 0px 0px;
+
+  margin: 5px 5px 5px 5px;
+  float: left;
+}
+
+</style>
