21c020f1fc9367a062a1f47fe2aa061c6b1eddda
Marios Kourouklis <Marios.Kourouklis@awacademy.training>
2022-12-09 10:52:52 +0200

    1

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <header></header>
+</div>
+</template>
+
+<script>
+import compHeader from './components/Header/Header.vue';
+export default {
+  components: {
+    compHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
